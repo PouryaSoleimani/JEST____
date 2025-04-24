863ceb20eac460c2164a569a1b5ccb2d724ff1be
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-24 21:35:36 +0330

    🟧 JEST

---
+++ js/cloneArray.test.js
@@ -0,0 +1,8 @@
+import cloneArray from "./cloneArray";
+
+test("CLONING AN ARRAY", () => {
+    expect(typeof (cloneArray)).toBe("function");
+    const array = [1, 2, 3, 4, 5];
+    expect(cloneArray(array)).not.toBe(array);
+    expect(cloneArray(array)).toStrictEqual(array)
+});
