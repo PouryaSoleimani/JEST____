0a362c3d0d49f1652df43b524d9b05bdb5e7c348
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-25 00:56:35 +0330

    � JEST

--- js/callback.test.js
+++ js/callback.test.js
@@ -3,10 +3,10 @@ import fetchData from './callback';
 test('fetchData calls the callback with the correct message', (done) => {
     function callback(data) {
         try {
-            expect(data).toBe('HELLO FROM CALLBACK');
-            done(); // Signal that the test is complete
+            expect(data).toBe("HELLO FROM CALLBACK");
+            done();
         } catch (error) {
-            done(error); // Fail the test if an error is thrown
+            done(error);
         }
     }
 
