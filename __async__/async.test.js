6b9c04ff00ba5dd60cbc16306a3f580b8442c031
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-26 01:34:34 +0330

    🟧 JEST

--- __async__/async.test.js
+++ __async__/async.test.js
@@ -2,9 +2,9 @@ import fetchData from "./async";
 
 // FETCHING DATA FROM API ===============================================================================================================================================
 describe("fetchData", () => {
-test("Should Return an Object" , () => {
-  expect(fetchData(1)).resolves.toBeInstanceOf(Object)
-})
+  test("Should Return an Object", () => {
+    expect(fetchData(1)).resolves.toBeInstanceOf(Object)
+  })
 });
 
 
--- __setup__/setup.js
+++ __setup__/setup.js
@@ -1,1 +1,9 @@
 const animals = ["cat", "dog", "mouse", "rabbit", "elephant"]
+
+
+
+describe("animals", () => {
+    test("Should Return an Array", () => { expect(animals).toBeInstanceOf(Array) })
+    test("Should Return an Array", () => { expect(animals).toHaveLength(5) });
+    test("Should add a new animal", () => { expect(animals.push("giraffe")).toHaveLength(6) });
+})
