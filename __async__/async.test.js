638ad5f01962efe74fc31aa13a971689b39ab9a4
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-26 00:04:03 +0330

    🟧 JEST

--- __async__/async.test.js
+++ __async__/async.test.js
@@ -1,10 +1,11 @@
 import fetchData from "./async";
 
-
+// FETCHING DATA FROM API ===============================================================================================================================================
 describe("fetchData", () => {
-    test("should return a promise", () => {
+    test("should return an object", () => {
         const SingleTodo = fetchData(1)
         expect(SingleTodo).resolves.toBe(Object)
     });
 });
+
 
