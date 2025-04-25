e263c2862bf6b8af2a31852078104f0509664025
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-25 22:03:31 +0330

    |� JEST

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -2,7 +2,10 @@ import sum from "../js/sum";
 
 
 test("should add two numbers", () => {
+
+  expect(typeof sum).toBe("function")
   const result = sum(1, 2);
   expect(result).toBe(3);
+
 });
     
