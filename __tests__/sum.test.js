4bcc16c747ac5f7450efeb0c6580997e2bdd40f9
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-25 22:09:14 +0330

    🟧 JEST

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -1,11 +1,15 @@
-import sum from "../js/sum";    
+import sum from "../js/sum";
 
+// test("should add two numbers", () => {
+//   expect(typeof sum).toBe("function")
+//   const result = sum(1, 2);
+//   expect(result).toBe(3);
+// });
 
-test("should add two numbers", () => {
-
-  expect(typeof sum).toBe("function")
-  const result = sum(1, 2);
-  expect(result).toBe(3);
-
-});
-    
+describe("sum", () => {
+  test("should add two numbers", () => {
+    expect(typeof sum).toBe("function")
+    const result = sum(1, 2);
+    expect(result).toBe(3);
+  });
+})
