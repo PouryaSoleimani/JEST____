988b9ef1565a0bce619c7aa9c9567a5d79c81ceb
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-25 23:51:55 +0330

    🟧 JEST

--- __async__/async.js
+++ __async__/async.js
@@ -1,14 +1,12 @@
 import axios from "axios";
 
 
-
-
 export default async function fetchData(id) {
     const response = await axios.get(`https://jsonplaceholder.typicode.com/todos/${id}`);
     console.log(response.data)
     return response.data;
 }
 
-fetchData(1)
+
 
 
