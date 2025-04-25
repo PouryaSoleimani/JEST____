e7217a434a7c4efcb1ae669834e0808c8088371c
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-25 21:20:25 +0330

    |� JEST

--- components/Article.tsx
+++ components/Article.tsx
@@ -12,10 +12,7 @@ export default function Article() {
           <h2 data-testid="heading" className="bg-blue-600 py-4 text-3xl text-center rounded">ARTICLE</h2>
           <Image src={logo} alt="lendSqr logo" className="w-28 md:w-56" />
           <Image src={signInHero} alt="lendSqr hero" className="hidden md:block" />
-          <Link
-            href="/test-page"
-            className="bg-orange-500 p-3 rounded text-white"
-          >
+          <Link href="/test-page" className="bg-orange-500 p-3 rounded text-white"    >
             LINK
           </Link>
         </div>
