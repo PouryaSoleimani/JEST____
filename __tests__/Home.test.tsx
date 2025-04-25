4db0a528a6a9991f8191e599c3838aae847efae9
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-25 15:22:41 +0330

    🟧 JEST

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -6,6 +6,8 @@ import '@testing-library/jest-dom';
 test("SHOULD HAVE THE WORD JEST IN THE DOM", () => {
     render(<Home />);
     // const myElement = screen.getByText("JEST TRAINING COURSE");
-    const myElement = screen.getByRole("heading");
+    const myElement = screen.getByRole("heading", {
+        name: "JEST TRAINING COURSE"
+    });
     expect(myElement).toBeInTheDocument();
-});  
+});   
