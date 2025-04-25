2c824ae0f8510591dc0ce5655578f9684de79517
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-25 22:10:49 +0330

    🟧 JEST

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -6,10 +6,14 @@ import sum from "../js/sum";
 //   expect(result).toBe(3);
 // });
 
+
+// ^ WE USE DESCRIBE WHEN WE WANT TO TEST MULTIPLE TESTS
 describe("sum", () => {
+  test("should be a function", () => {
+    expect(typeof sum).toBe("function");
+  });
   test("should add two numbers", () => {
-    expect(typeof sum).toBe("function")
     const result = sum(1, 2);
     expect(result).toBe(3);
-  });
+  })
 })
