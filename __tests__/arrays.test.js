fe35de951d261c3fa4edb63cb8d66b718999f82a
Pourya_Delivando <Pourya@delivando.com>
2025-05-05 22:25:59 -0400

    JEST

--- __tests__/arrays.test.js
+++ __tests__/arrays.test.js
@@ -4,6 +4,6 @@ import arrayTest from "../__JS__/arrays"
 describe('TESTING AN ARRAY', () => { 
     const result = arrayTest();
     test('should render the array correctly', () => { 
-        expect(result).toContain("cake")
+        expect(result).toContain("egg")
      })
  })
