9ad9ae7e90ae9d03239699f0ae096a225c2cdaab
Pourya_Delivando <Pourya@delivando.com>
2025-05-05 21:43:54 -0400

    JEST

--- __tests__/gte.test.js
+++ __tests__/gte.test.js
@@ -2,6 +2,7 @@ import gte from "../__JS__/gte"
 
 describe('GREATER THAN TEST', () => { 
     it("SHOULD RETURN TRUE WHEN ITS GREATER" , () => {
-      expect(gte(5,2)).toBeTruthy()
+      expect(gte(5,2)).toBeTruthy();
+      expect(gte(5,2)).not.toBeFalsy()
     })
  })
