472fbc64e3fe310967a2b64e1a466e6b8a457b1c
Pourya_Delivando <Pourya@delivando.com>
2025-05-05 20:33:28 -0400

    JEST

--- app/jest/page.tsx
+++ app/jest/page.tsx
@@ -12,4 +12,4 @@ export default page;
 
 // This is a simple React functional component that renders a heading "JEST TUTORIAL" inside a div.
 // It uses TypeScript for type safety by defining the component as a React.FC (Functional Component).
-// The component is exported as the default export of the module, allowing it to be imported and used in other parts of the application.
+// The component is exported as the default export of the module, allowing it to be imported and used in other parts of the application.ّ
