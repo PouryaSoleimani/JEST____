3987aa608da156bf97105bdc878a385131fbdb35
Pourya_Delivando <Pourya@delivando.com>
2025-05-05 20:31:47 -0400

    JEST

--- app/jest/page.tsx
+++ app/jest/page.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
-const page = () => {
+const page : React.FC= () => {
     return (
         <div>
-
+            <h1>JEST TUTORIAL</h1>
         </div>
     );
 };
 
 export default page;
+
+// This is a simple React functional component that renders a heading "JEST TUTORIAL" inside a div.
+// It uses TypeScript for type safety by defining the component as a React.FC (Functional Component).
+// The component is exported as the default export of the module, allowing it to be imported and used in other parts of the application.
