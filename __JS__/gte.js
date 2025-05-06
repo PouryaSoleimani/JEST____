d40fffd26f25e1c4dcecca389ab5f4933b583cae
Pourya_Delivando <Pourya@delivando.com>
2025-05-05 21:43:08 -0400

    JEST

---
+++ __JS__/gte.js
@@ -0,0 +1,7 @@
+export default  function gte(a , b) {
+    if(a > b ) {
+        return true
+    } else{
+        return false
+    }
+}
---
+++ __tests__/gte.test.js
@@ -0,0 +1,7 @@
+import gte from "../__JS__/gte"
+
+describe('GREATER THAN TEST', () => { 
+    it("SHOULD RETURN TRUE WHEN ITS GREATER" , () => {
+      expect(gte(5,2)).toBeTruthy()
+    })
+ })
