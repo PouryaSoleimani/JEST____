eb12fac73163b7cfd0fb1ef602258f824df328fa
Pourya_Delivando <Pourya@delivando.com>
2025-05-08 04:10:04 -0400

    JEST

--- __tests__/objects.test.js
+++ __tests__/objects.test.js
@@ -1,1 +1,21 @@
 import objects from "../__JS__/objects";
+
+
+describe('OBJECT TESTING', () => { 
+    it("should return an object with specific properties", () => {
+        const obj = objects();
+        expect(obj).toHaveProperty('id');
+        expect(obj).toHaveProperty('name');
+        expect(obj).toHaveProperty('age');
+        expect(obj).toHaveProperty('occupation');
+        expect(obj).toHaveProperty('married');
+        expect(obj).toHaveProperty('hobbies');
+        expect(obj).toHaveProperty('address');
+        expect(obj).toHaveProperty('skills');
+        expect(obj).toHaveProperty('experience');
+        expect(obj).toHaveProperty('education');
+        expect(obj).toHaveProperty('languages');
+        expect(obj).toHaveProperty('certifications');
+        expect(obj).toHaveProperty('projects');
+    });
+})
