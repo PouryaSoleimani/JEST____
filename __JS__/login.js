80a830186b64337cb2d6bf64bfb1fc30cf4772f0
Pourya_Delivando <Pourya@delivando.com>
2025-05-08 04:25:38 -0400

    JEST

---
+++ __JS__/login.js
@@ -0,0 +1,9 @@
+//^ LOGIN FUNCTION
+function loginHandler(password) {
+    if (password === "1234") {
+        alert("Login successful!");
+    }
+    else {
+        alert("Incorrect password. Please try again.");
+    }
+}
