76a280acac9244a04ceb2ac5ad3cb6718579f435
Pourya_Delivando <Pourya@delivando.com>
2025-05-10 16:10:31 -0400

    JESSSSST

---
+++ __JS__/mocks.js
@@ -0,0 +1,20 @@
+
+
+export function getUserByID(id) {
+console.info("READING FROM DATABASE")
+const users = [
+	{ id: 1, name: 'John Doe', email: 'john@example.com' },
+	{ id: 2, name: 'Jane Doe', email: 'jane@example.com' }
+]
+return users.find(user => user.id === id)
+}
+
+export function sendEmail(name, message) {
+console.info(`Sending email to ${name} with message: ${message}`)
+}
+
+export function sendWelcomeEmail(name, email) {
+console.info(`Sending welcome email to ${name} at ${email}`)
+const message = 'Welcome to our platform!'
+sendEmail(name, message)
+}
