4c4d0ed0d84b40083612010b5b5a94e2e463900a
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-05-11 01:08:53 -0400

    JEST

--- __tests__/mock__code.test.js
+++ __tests__/mock__code.test.js
@@ -3,30 +3,32 @@
 import { it,expect,describe } from '@jest/globals'
 import jest from 'jest-mock';
 import { calculateDiscount } from '../__JS__/mock__code';
-import { getUserById, sendEmail } from '../__JS__/mock__service';
-import { get } from 'http';
+import { getUserById } from "./mock__service";
+import { sendEmail } from "./mock__service";
+
+
 
 // ^ MOCKING THE FUNCTIONS
-jest.mock('../__JS__/mock__service', () => ({
-	sendEmail: jest.fn(),
-	getUserById: jest.fn().mockReturnValue({
-		id: 1,
-		name: 'John',
-		email: 'john@gmail.com',
-		purchaseCount: 6
-	})
-}))
+// jest.mock('../__JS__/mock__service', () => ({
+// 	sendEmail: jest.fn(),
+// 	getUserById: jest.fn().mockReturnValue({
+// 		id: 1,
+// 		name: 'John',
+// 		email: 'john@gmail.com',
+// 		purchaseCount: 6
+// 	})
+// }))
 
 
 describe('calculateDiscount', () => {
 	it('SHOULD RETURN 10% DISCOUNT', () => {
-	sendEmail: jest.fn();
-    getUserById: jest.fn().mockReturnValue({
-      id: 1,
-      name: 'John',
-      email: 'john@gmail.com',
-      purchaseCount: 6
-    })
+	sendEmail = jest.fn()
+    getUserById = jest.fn().mockReturnValue({
+			id: 1,
+			name: 'John',
+			email: 'john@gmail.com',
+			purchaseCount: 6
+		})
     expect(calculateDiscount(1)).toBe(10)
 	})
 })
