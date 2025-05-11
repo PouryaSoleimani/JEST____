f5f274f7f5bbc9444c55eb4d1d81b936a937dcaf
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-05-11 01:14:59 -0400

    JEST

--- __tests__/mock__code.test.js
+++ __tests__/mock__code.test.js
@@ -1,41 +1,26 @@
-/* eslint-disable no-unused-labels */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { it,expect,describe } from '@jest/globals'
 import jest from 'jest-mock';
 import { calculateDiscount } from '../__JS__/mock__code';
-import { getUserById } from "./mock__service";
-import { sendEmail } from "./mock__service";
+import { getUserById, sendEmail } from "./mock__service";
 
 
 
 // ^ MOCKING THE FUNCTIONS
-// jest.mock('../__JS__/mock__service', () => ({
-// 	sendEmail: jest.fn(),
-// 	getUserById: jest.fn().mockReturnValue({
-// 		id: 1,
-// 		name: 'John',
-// 		email: 'john@gmail.com',
-// 		purchaseCount: 6
-// 	})
-// }))
+jest.mock('./mock__service', () => ({
+	sendEmail: jest.fn(),
+	getUserById: jest.fn().mockReturnValue({
+		id: 1,
+		name: 'John',
+		email: 'john@gmail.com',
+		purchaseCount: 6
+	})
+}));
 
 
 describe('calculateDiscount', () => {
 	it('SHOULD RETURN 10% DISCOUNT', () => {
-	sendEmail = jest.fn()
-    getUserById = jest.fn().mockReturnValue({
-			id: 1,
-			name: 'John',
-			email: 'john@gmail.com',
-			purchaseCount: 6
-		})
-    expect(calculateDiscount(1)).toBe(10)
-	})
+	expect(calculateDiscount(1)).toBe(10)
+})
 })
 
-
-it('SHOULD SEND AN EMAIL TO THE USER', () => {})
-it('SHOULD NOT SEND AN EMAIL TO THE USER', () => {})
-
-
-  
