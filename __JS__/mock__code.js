7197107cf5957959af2bb25c1e68d4cb110532bd
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-05-11 01:39:45 -0400

    JEST

--- __JS__/mock__code.js
+++ __JS__/mock__code.js
@@ -1,12 +1,12 @@
 import { getUserById } from "./mock__service";
-// import { sendEmail } from "./mock__service";
+import { sendEmail } from "./mock__service";
 
 export function calculateDiscount (id){
 	const user = getUserById(id)
 	let discount = 0
 	if (user.purchaseCount > 3) {
 		discount = 10
-		// sendEmail(user.name, `You have a discount of ${discount}%`)
+		sendEmail(user.name, `You have a discount of ${discount}%`)
 		return discount
 	} else {
 		return 0
